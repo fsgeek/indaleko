0ffb8df6c6dbaf2c14a628d24b21317da91f0fe5
Tony Mason <tony@fsgeek@cs.ubc.ca>
2025-05-03 18:58:14 -0700

    Define package exports for task_activity and import TaskActivityCollector in __all__

--- activity/collectors/task_activity/__init__.py
+++ activity/collectors/task_activity/__init__.py
@@ -17,3 +17,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+# Package activity.collectors.task_activity
+from activity.collectors.task_activity.task_collector import TaskActivityCollector
+
+__all__ = ["TaskActivityCollector"]
